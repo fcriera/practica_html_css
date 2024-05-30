4fffc6fbf6277d01aec2a9cf20e85cc2f6789d92
Francisco Chávez <fcojavierchavez98@gmail.com>
2024-05-30 16:24:17 -0500

    Se realiza la parte de Js, el menu de inicio redirige

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
                     <a href="#galeria">Galeria</a>
                     <a href="#contactenos">Contactenos</a>
                 </nav>
+                <a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
             </div>
         </header>
         <main>
@@ -47,7 +48,7 @@
             <section id="servicios" class="seccion">
                 <div class="container">
                     <div class="row">
-                        <div class="columna columna-33">
+                        <div class="columna columna-33 columna-mobile-100">
                             <div class="bloque-servicio">
                                 <div class="bloque-img-servicio cuadrado-perfecto">
                                     <img src="img/servicio1.jpg">
@@ -62,7 +63,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="columna columna-33">
+                        <div class="columna columna-33 columna-mobile-100">
                             <div class="bloque-servicio">
                                 <div class="bloque-img-servicio cuadrado-perfecto">
                                     <img src="img/servicio1.jpg">
@@ -78,7 +79,7 @@
                             </div>
 
                         </div>
-                        <div class="columna columna-33">
+                        <div class="columna columna-33 columna-mobile-100">
                             <div class="bloque-servicio">
                                 <div class="bloque-img-servicio cuadrado-perfecto">
                                     <img src="img/servicio1.jpg">
@@ -100,37 +101,37 @@
             <section id="galeria">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="columna-33">
+                        <div class="columna-33 columna-mobile-50">
                             <div class="cuadrado-perfecto">
                                 <img src="img/servicio1.jpg">
                                 <h4>Imagen1</h4>
                             </div>
                         </div>
-                        <div class="columna-33">
+                        <div class="columna-33 columna-mobile-50">
                             <div class="cuadrado-perfecto">
                                 <img src="img/servicio2.jpg">
                                 <h4>Imagen2</h4>
                             </div>
                         </div>
-                        <div class="columna-33">
+                        <div class="columna-33 columna-mobile-50">
                             <div class="cuadrado-perfecto">
                                 <img src="img/servicio3.jpg">
                                 <h4>Imagen3</h4>
                             </div>
                         </div>
-                        <div class="columna-33">
+                        <div class="columna-33 columna-mobile-50">
                             <div class="cuadrado-perfecto">
                                 <img src="img/servicio4.jpg">
                                 <h4>Imagen4</h4>
                             </div>
                         </div>
-                        <div class="columna-33">
+                        <div class="columna-33 columna-mobile-50">
                             <div class="cuadrado-perfecto">
                                 <img src="img/servicio5.jpg">
                                 <h4>Imagen5</h4>
                             </div>
                         </div>
-                        <div class="columna-33">
+                        <div class="columna-33 columna-mobile-50">
                             <div class="cuadrado-perfecto">
                                 <img src="img/servicio6.jpg">
                                 <h4>Imagen6</h4>
@@ -143,7 +144,7 @@
                 <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Guayaquil+(Lima)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='http://maps-website.com/es'>más información en maps-generator.com</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=f26433dac75a29c835257c9bd1f02eedb989d06f'></script> 
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="columna columna-33 empujar-66">
+                        <div class="columna columna-33 columna-mobile-100 empujar-66 empujar-mobile-0 sinpadding-mobile">
                             <form action="" method="post">
                                 <div class="form-block">
                                     <input type="text" name="nombre" class="form-control" placeholder="Nombre">
@@ -166,11 +167,11 @@
         <footer>
             <div class="container">
                 <div class="row">
-                    <div class="columna columna-25">
+                    <div class="columna columna-25 columna-mobile-100">
                         <img src="img/logo-blanco.png" class="logo-footer">
                         <p>Duis aute irure dolor in</p>
                     </div>
-                    <div class="columna columna-25">
+                    <div class="columna columna-25 columna-mobile-100">
                         <h3>
                             Temas relacionados
                         </h3>
@@ -180,7 +181,7 @@
                             <li><a href="#">Tema 3</a></li>
                         </ul>
                     </div>
-                    <div class="columna columna-25">
+                    <div class="columna columna-25 columna-mobile-100">
                         <h3>
                             Datos de contacto
                         </h3>
@@ -190,7 +191,7 @@
                             <li>La Floresta<br>Quito - Ecuador</li>
                         </ul>
                     </div>
-                    <div class="columna columna-25">
+                    <div class="columna columna-25 columna-mobile-100">
                         <h3>
                             Redes Sociales
                         </h3>
@@ -206,5 +207,7 @@
                 &copy; Derechos Reservados - 2020
             </div>
         </footer>
+        <script src="js/jquery.js"></script>
+        <script src="js/funciones.js"></script>
     </body>
 </html>
